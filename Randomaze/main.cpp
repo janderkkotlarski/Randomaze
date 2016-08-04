30e971c997a20bb45aff9c309f00e06f6b7b654d
janderkkotlarski <j.d.kotlarski@gmx.com>
2016-08-04 17:32:16 +0200

    new

--- Randomaze/main.cpp
+++ Randomaze/main.cpp
@@ -5,11 +5,67 @@
 
 #include <SFML/Graphics.hpp>
 
+class pattern
+{
+    const std::string m_filename{"Tile.png"};
+
+    sf::Color m_color;
+
+    sf::Vector2f m_posit;
+
+    sf::Texture m_texture;
+
+    sf::Sprite m_sprite;
+
+    void set_filename()
+    {
+        assert(m_filename != "");
+
+        if (!m_texture.loadFromFile(m_filename))
+        {
+            std::cout << m_filename << " not found!\n";
+        }
+    }
+
+    void set_texture()
+    {
+        m_sprite.setTexture(m_texture);
+    }
+
+    void set_color()
+    {
+        m_sprite.setColor(m_color);
+    }
+
+    void set_posit()
+    {
+        m_sprite.setPosition(m_posit);
+    }
+
+    public:
+
+    void reposit(const sf::Vector2f& posit)
+    {
+        m_posit = posit;
+    }
+
+    void recolor(const sf::Color& color)
+    {
+        m_color = color;
+        set_color();
+    }
+
+    void display(sf::RenderWindow& window)
+    {
+        window.draw(m_sprite);
+    }
+};
+
 class tile
 {
     const sf::Vector2i m_posit;
 
-    const std::string m_tile_name{"Tile.png"};
+
 
     const sf::Color m_dark{63, 63, 63};
     const sf::Color m_grey{127, 127, 127};
@@ -61,7 +117,7 @@ class tile
     tile(const sf::Vector2i& posit, const int type)
         : m_posit(posit), m_type(type)
     {
-
+        color_tile();
     }
 
     ~tile()
